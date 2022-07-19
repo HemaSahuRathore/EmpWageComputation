d6625421625567022be5f9802a144c58a119334e
Hema <hemasahu019@gmail.com>
2022-07-19 18:05:29 +0530

    [Hema Added Welcome Page]

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace D4_EmpWageComputation
+{   /// <summary>
+    /// Welcome page
+    /// </summary>
+    internal class Program
+    {   //An entrypoint of console app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation Assignment Program"); //Welcome code
+            Console.ReadLine();
+        }
+    }
+}
