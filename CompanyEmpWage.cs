fbb72ab55738115492976deaa30b76652d8c797c
Hema <hemasahu019@gmail.com>
2022-07-22 23:16:46 +0530

    [Added UC10 Ability to manage employee Wage of multiple companies]

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4_EmpWageComputation
+{   
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int wagePerHour, maxNumWorkingDays, maxHour, totalWage;
+        public CompanyEmpWage(string company, int wagePerHour, int maxNumWorkingDays, int maxHour)
+        {
+            this.company = company;
+            this.wagePerHour = wagePerHour;
+            this.maxNumWorkingDays = maxNumWorkingDays;
+            this.maxHour = maxHour;
+        }
+
+        public void SetTotalWage(int totalWage)
+        {
+            this.totalWage = totalWage;
+        }
+
+        public override string ToString()
+        {
+            return "\nCompany: " + this.company + "  Total Wage: " + this.totalWage;
+        }
+
+    }
+}
--- EmpWageCalculation.cs
+++ EmpWageCalculation.cs
@@ -8,66 +8,75 @@ namespace D4_EmpWageComputation
 {
     public class EmpWageCalculation
     {
-        string company;
-        int wagePerHour;
-        int maxNumWorkingDays;
-        int maxHour;
-        int totalWage;
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+         
+        int numOfCompany = 0;
+        public CompanyEmpWage[] companyEmpWageArray; //Declare CompEmpWage type array
 
-        const int IS_FULL_TIME = 1;
-        const int IS_PART_TIME = 2;
-
-        //Constructor
-        public EmpWageCalculation(string company, int wagePerHour, int maxNumWorkingDays, int maxHour)
+        public EmpWageCalculation()
         {
-            this.company = company;
-            this.wagePerHour = wagePerHour;
-            this.maxNumWorkingDays = maxNumWorkingDays;
-            this.maxHour = maxHour;
+            this.companyEmpWageArray = new CompanyEmpWage[5];
         }
 
-        //Method to calculate Emp wage
-        public void CalEmpWage()
+        public void AddCompanyWage(string company, int wagePerHour, int maxNumWorkingDays, int maxHour)
         {
+            //companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, wagePerHour, maxNumWorkingDays, maxHour);
+            //numOfCompany++;
+            CompanyEmpWage companyEmpWageObj = new CompanyEmpWage( company, wagePerHour, maxNumWorkingDays, maxHour);
+            companyEmpWageArray[this.numOfCompany] = companyEmpWageObj;
+            numOfCompany++;
 
-            int empHour = 0, empWage = 0; 
+        }
+
+        public void ComputeEmpWage()
+        {
+            for(int i = 0; i < numOfCompany; i++)
+            {
+                int totalWage = ComputeEmpWage(companyEmpWageArray[i]);
+                companyEmpWageArray[i].SetTotalWage(totalWage);
+                Console.WriteLine("Companies Details : " + companyEmpWageArray[i]);
+               
+            }
+        }
+
+        public int ComputeEmpWage(CompanyEmpWage CompanyEmpWage)
+        {
+            int empHour = 0, empWage = 0; int totalWage = 0;
             int day = 1;
             int workingHour = 0;
+
             Random random = new Random();
-            //using while loop as we are not aware as how many iteration is required to get the desired output
-            while (day <= maxNumWorkingDays && workingHour < maxHour)
+            
+            while (day <= CompanyEmpWage.maxNumWorkingDays && workingHour < CompanyEmpWage.maxHour)
             {
-                int empCheck = random.Next(0, 3); 
-               
+                int empCheck = random.Next(0, 3);
+                 
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
-                        empHour = 8;
-                        break;
+                            empHour = 8;
+                            break;
 
                     case IS_PART_TIME:
-                        empHour = 4; 
-                        break;
+                            empHour = 4;
+                            break;
 
                     default:
-                        empHour = 0;
-                        break;
+                            empHour = 0;
+                            break;
                 }
 
                 workingHour += empHour;
-                empWage = empHour * wagePerHour; //Calculating Emp Wage( = Emp hour * wage per hour) for one day
-                Console.WriteLine("Employee wage for day-{0} is {1}", day, empWage); //displaying Emp wage
-                totalWage += empWage;  //total wage for max day or max hour
-                day++; //incrementing day after every iteration and to avoid infinite while loop
+                empWage = empHour * CompanyEmpWage.wagePerHour; 
+                Console.WriteLine("Employee wage for day-{0} is {1}", day, empWage); 
+                totalWage = totalWage + empWage;  
+                day++; 
+
             }
-            Console.WriteLine("\nTotal Wage of {0} for {1} day (working hour {2}) is {3}", company, day - 1, workingHour, totalWage);
-           
 
-    }
-        public override string ToString()
-        {
-            return "\nCompany: " + this.company + "  Total wage: " + this.totalWage;
+            return totalWage;
         }
-
+          
     }
 }
